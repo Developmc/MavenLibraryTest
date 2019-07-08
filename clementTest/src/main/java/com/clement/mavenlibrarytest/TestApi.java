148e35fb5fd1f4ecec765071f19dd7fe7195c0b6
TPClement <>
2019-07-08 15:54:20 +0800

    First commit

--- clementTest/src/main/java/com/clement/mavenlibrarytest/TestApi.java
+++ clementTest/src/main/java/com/clement/mavenlibrarytest/TestApi.java
@@ -3,6 +3,8 @@ package com.clement.mavenlibrarytest;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 /**
  * Author: tp-clement
  * Create: 2019/7/5
@@ -14,6 +16,9 @@ public class TestApi {
      * 测试提示
      */
     public static void doTest(Context context) {
-        Toast.makeText(context, "Hello world", Toast.LENGTH_LONG).show();
+        //测试第三方远程依赖库
+        String userJson = "{'name':'clement'}";
+        User user = new Gson().fromJson(userJson, User.class);
+        Toast.makeText(context, user.getName(), Toast.LENGTH_LONG).show();
     }
 }
---
+++ clementTest/src/main/java/com/clement/mavenlibrarytest/User.java
@@ -0,0 +1,18 @@
+package com.clement.mavenlibrarytest;
+
+/**
+ * Author: tp-clement
+ * Create: 2019/7/8
+ * Desc:
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
